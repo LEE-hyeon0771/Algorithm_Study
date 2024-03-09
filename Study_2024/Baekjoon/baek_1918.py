2ef9708184f60cdf72430a71427e12932a5291a6
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2024-03-09 16:21:49 +0900

    1918

---
+++ Study_2024/Baekjoon/baek_1918.py
@@ -0,0 +1,26 @@
+str = list(input())
+stack = []
+result = ''
+for i in str:
+    if i.isalpha():
+        result += i
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i =='/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(i)
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(i)
+        elif i == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+
+while stack:
+    result += stack.pop()
+print(result)
+                
