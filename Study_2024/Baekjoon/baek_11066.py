a6c6dad90a5a8f49931370c3ca91c72b7c11e6f2
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2024-06-26 15:23:14 +0900

    11758

--- Study_2024/Baekjoon/baek_11066.py
+++ Study_2024/Baekjoon/baek_11066.py
@@ -1,3 +1,6 @@
+# dp : 두 파일을 합칠 때 최소 비용 저장
+# sum_matrix : 파일 크기의 부분 합 저장
+
 import sys
 input = sys.stdin.readline
 
@@ -6,11 +9,12 @@ t = int(input().strip())
 for _ in range(t):
     k = int(input().strip())
     arr = list(map(int, input().strip().split()))
-
+    
+    # total : 파일 크기의 누적합
     total = [0] * (k + 1)
     for i in range(1, k + 1):
         total[i] = total[i - 1] + arr[i - 1]
-
+    
     dp = [[0] * k for _ in range(k)]
     sum_matrix = [[0] * k for _ in range(k)]
 
@@ -21,9 +25,14 @@ for _ in range(t):
 
     for length in range(2, k + 1):
         for i in range(k - length + 1):
-            j = i + length - 1
+            j = i + length - 1   # 부분 배열의 끝 인덱스
             dp[i][j] = float('inf')
             for mid in range(i, j):
                 dp[i][j] = min(dp[i][j], dp[i][mid] + dp[mid + 1][j] + sum_matrix[i][j])
 
     print(dp[0][k - 1])
+    
+    
+    # 40 30 30 50 
+    
+# mid = 1 -> 40 30 / 30 50  -> 70 + 80 + 150 =  300
